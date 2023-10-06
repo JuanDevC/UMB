cbcae3c3558eeb00856d7e9e626d8771d3c75a0c
david0-012 <davidcasallas1202@gmail.com>
2023-10-06 09:54:57 -0500

    Clase 10 Django
    (Clase 09 fue parcial) Se arregla el orden de las carpetas, se implementa diseño de bootstrap, se modifca settings,  se modifican todos los archivos y se crean carpetas nuevas y archivos nuevos para su funcionamiento.

--- Clase07/djClean/settings.py
+++ Clase07/djClean/settings.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import os   
 """
 Django settings for djClean project.
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aplicaciones.proyectos',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +60,7 @@ SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'templates'),],
+        "DIRS": [ os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
